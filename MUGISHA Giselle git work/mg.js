164c21bd872a01400231554eb5290cb433c2d760
Mugiselle <126867118+Mugiselle@users.noreply.github.com>
2023-06-12 19:20:26 +0200

    my first commit

---
+++ MUGISHA Giselle git work/mg.js
@@ -0,0 +1,25 @@
+let input = document.querySelector("input")
+let buttons = document.querySelectorAll("button")
+let equal = document.getElementById("equal")
+let zero = document.getElementById("zero")
+let del = document.getElementById("del")
+
+buttons.forEach(button => {
+    button.onclick = function () {
+        input.value += button.textContent;
+    }
+})
+
+function sideoperators() {
+   equal.onclick = function(){
+    input.value = eval(input.value)
+   } 
+   del.onclick = function(){
+    input.value = "";
+   }
+   zero.onclick = function(){
+    input.value = 0;
+   }
+}
+
+sideoperators()
